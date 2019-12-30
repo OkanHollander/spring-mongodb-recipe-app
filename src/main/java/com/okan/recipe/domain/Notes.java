66812f55fbb2d5589e0d8932a79dd27ab7558358
Okan Hollander <o.hollander1990@gmail.com>
2019-12-30 13:27:23 +0100

    added Notes and Recipe

---
+++ src/main/java/com/okan/recipe/domain/Notes.java
@@ -0,0 +1,48 @@
+package com.okan.recipe.domain;
+
+import javax.persistence.*;
+
+/**
+ * Author:   Okan Hollander
+ * Date:     30/12/2019
+ * Time:     13:06
+ */
+@Entity
+public class Notes {
+
+    // == Fields ==
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipeNotes;
+
+    // == Getters and Setters ==
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
