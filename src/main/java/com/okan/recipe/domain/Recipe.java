2ae8fb7b24a6a4198069f94c501a270544b3c535
Okan Hollander <o.hollander1990@gmail.com>
2019-12-31 13:20:34 +0100

    polished project

--- src/main/java/com/okan/recipe/domain/Recipe.java
+++ src/main/java/com/okan/recipe/domain/Recipe.java
@@ -132,6 +132,7 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+        notes.setRecipe(this);
     }
 
     public Set<Ingredient> getIngredients() {
@@ -149,4 +150,19 @@ public class Recipe {
     public void setCategories(Set<Category> categories) {
         this.categories = categories;
     }
+
+    // == methods ==
+    public void addIngredient(Ingredient ingredient) {
+        ingredient.setRecipe(this);
+        this.ingredients.add(ingredient);
+
+    }
+//    public Recipe addIngredient(Ingredient ingredient) {
+//        ingredient.setRecipe(this);
+//        this.ingredients.add(ingredient);
+//
+//        return this;
+//    }
+
+
 }
